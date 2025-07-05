1cd1346a9162b8021809ae8186303144ab30c94c
n7kel <notnotnot00000@mail.ru>
2025-07-06 01:54:37 +0300

    server fix

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function connect() {
     // ВАЖНО: Этот адрес нужно будет заменить на публичный адрес от Cloudflare
-    const websocketUrl = "wss://402e-83-172-150-93.ngrok-free.app"; 
+    const websocketUrl = "wss://4ed4-83-172-150-93.ngrok-free.ap";
     
     ws = new WebSocket(websocketUrl);
 
